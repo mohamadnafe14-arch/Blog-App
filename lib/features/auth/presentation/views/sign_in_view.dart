1456967d1fffea0d43d3a7301bc12e9c7a361d71
mohamed <mnafe44@gmail.com>
2026-02-06 03:20:53 +0200

    adding the app theme

--- lib/features/auth/presentation/views/sign_in_view.dart
+++ lib/features/auth/presentation/views/sign_in_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text(
+            'Sign In View',
+            style: TextStyle(fontSize: 50.sp, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/theme/app_theme.dart';
 import 'package:blog_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -15,9 +16,10 @@ class BlogApp extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) =>  MaterialApp.router(
+      builder: (context, child) => MaterialApp.router(
         routerConfig: AppRouter.appRouter,
         debugShowCheckedModeBanner: false,
+        theme: AppTheme.appTheme,
       ),
     );
   }
