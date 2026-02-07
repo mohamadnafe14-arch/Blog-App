8b14fa48a4c72e4cdafdcbc7e8dc4fcc874f98dd
mohamed <mnafe44@gmail.com>
2026-02-07 14:06:40 +0200

    adding the supabase database

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+abstract class AppSecrets {
+  static const supaBaseUrl = "https://syohtbgtkydwhambejuo.supabase.co";
+  static const supaKey = "sb_publishable_xV_7Mzqgz0b0f5ggHB13Bg_oO5eucyz";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/app_theme.dart';
 import 'package:blog_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+ final supabase = await Supabase.initialize(
+    url: AppSecrets.supaBaseUrl,
+    anonKey: AppSecrets.supaKey,
+  );
   runApp(const BlogApp());
 }
 
