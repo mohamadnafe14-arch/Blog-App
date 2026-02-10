cb551303c00a2fc32e565d4138331146e1d2cbca
mohamed <mnafe44@gmail.com>
2026-02-10 07:11:32 +0200

    adding auth repo implemetation

---
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,44 @@
+import 'package:blog_app/core/errors/exceptions.dart';
+import 'package:blog_app/core/errors/failure.dart';
+import 'package:blog_app/features/auth/domain/data_sources/remote_auth_data_source.dart';
+import 'package:blog_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  final RemoteAuthDataSource remoteAuthDataSource;
+
+  AuthRepoImpl(this.remoteAuthDataSource);
+  @override
+  Future<Either<Failure, String>> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final res = await remoteAuthDataSource.signIn(
+        email: email,
+        password: password,
+      );
+      return Right(res);
+    } on ServerException catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, String>> signUp({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      final res = await remoteAuthDataSource.signUp(
+        email: email,
+        password: password,
+        name: name,
+      );
+      return Right(res);
+    } on ServerException catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+}
