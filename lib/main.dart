4d29f315bac53ad9acd62c7deacd70ec8fb61a96
mohamed <mnafe44@gmail.com>
2026-02-10 06:20:33 +0200

    adding exceptions class

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
- final supabase = await Supabase.initialize(
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
     url: AppSecrets.supaBaseUrl,
     anonKey: AppSecrets.supaKey,
   );
