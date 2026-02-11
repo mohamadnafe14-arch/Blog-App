443bc0cd3e9d1f62250d21e938f96eec51f8f334
mohamed <mnafe44@gmail.com>
2026-02-11 10:23:59 +0200

    adding user model

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,8 @@
+import 'package:blog_app/features/auth/domain/entities/user.dart';
+
+class UserModel extends User {
+  UserModel({required super.id, required super.email, required super.name});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      UserModel(id: json['id'], email: json['email'], name: json['name']);
+}
