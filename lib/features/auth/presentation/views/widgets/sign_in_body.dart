083e95baecd490da781151802824f48994d6ba56
mohamed <mnafe44@gmail.com>
2026-02-11 10:51:24 +0200

    finshing sign up page

--- lib/features/auth/presentation/views/widgets/sign_in_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_body.dart
@@ -75,7 +75,14 @@ class _SignInBodyState extends State<SignInBody> {
                 },
               ),
               SizedBox(height: 20.h),
-              BlocBuilder<AuthCubit, AuthState>(
+              BlocConsumer<AuthCubit, AuthState>(
+                listener: (context, state) {
+                  if (state is AuthError) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(state.message)),
+                    );
+                  }
+                },
                 builder: (context, state) {
                   final isLoading = state is AuthLoading;
                   return AuthButton(
