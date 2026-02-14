05de86374110d4962cc37550dc8939891677af7a
mohamed <mnafe44@gmail.com>
2026-02-14 12:48:32 +0200

    finshing blog cubit

--- lib/features/blog/presentation/manager/blog_cubit/blog_cubit.dart
+++ lib/features/blog/presentation/manager/blog_cubit/blog_cubit.dart
@@ -1,10 +1,34 @@
+import 'dart:io';
 
-
+import 'package:blog_app/features/blog/domain/usecases/add_blog_use_case.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'blog_state.dart';
 
 class BlogCubit extends Cubit<BlogState> {
-  BlogCubit() : super(BlogInitial());
+  final AddBlogUseCase addBlogUseCase;
+  BlogCubit(this.addBlogUseCase) : super(BlogInitial());
+  Future<void> addBlog({
+    required String title,
+    required String posterId,
+    required String content,
+    required File image,
+    required List<String> topics,
+  }) async {
+    emit(BlogLoading());
+    final result = await addBlogUseCase.call(
+      AddBlogUseCaseParams(
+        title: title,
+        posterId: posterId,
+        content: content,
+        image: image,
+        topics: topics,
+      ),
+    );
+    result.fold(
+      (l) => emit(BlogFailure(l.errMessage)),
+      (r) => emit(BlogSuccess()),
+    );
+  }
 }
